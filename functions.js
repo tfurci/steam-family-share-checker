c48b7493ebfb0384b18ce397383533d9cc9d9fee
tfurci <48729601+tfurci@users.noreply.github.com>
2024-06-05 15:47:29 +0200

    Fix status indicator

--- functions.js
+++ functions.js
@@ -26,10 +26,11 @@ async function fetchWithRetries(url, maxTotalTime = 5000, responseType = 'text')
 
 async function checkAppStatus() {
     try {
-        const response = await fetch('https://13584595.xyz/status');
-        const data = await response.json();
+        const response = await fetch('http://localhost:3000/status');
+        const text = await response.text(); // Get the text content of the response
+        console.log('Response:', text); // Log the response for debugging
         const fetchstatusIndicator = document.getElementById('fetchstatusIndicator');
-        if (data.active === true) {
+        if (text === 'OK') {
             fetchstatusIndicator.textContent = '🟢'; // Green Circle
         } else {
             fetchstatusIndicator.textContent = '🔴'; // Red Circle
